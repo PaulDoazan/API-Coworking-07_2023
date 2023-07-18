a2663e7213005c7f834e83c909847330a59b3386
pdoazan <paul.doazan@gmail.com>
2023-07-18 12:56:32 +0200

    Setup sequelize

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('coworking_07_2023', 'root', '', {
+    host: 'localhost',
+    dialect: 'mariadb',
+    logging: false
+});
+
+sequelize.authenticate()
+    .then(() => console.log('La connexion à la base de données a bien été établie.'))
+    .catch(error => console.log(`Ìmpossible de se connecter à la base de données ${error}`))
+
+const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+});
+
+sequelize.sync()
+
 app.use(morgan('dev'))
 app.use(express.json())
 
